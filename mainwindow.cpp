fa3e8ea16302c2b3d59acad121c71dbf1c1e93e9
whiskeyAndrew <andrewahti@gmail.com>
2022-03-12 14:20:39 +0300

    Added QFIleDialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,16 +7,16 @@
 #include <QDebug>
 #include <QFileInfo>
 #include <QDir>
+#include <QFileDialog>
 
 
-QString fileName;
+QString fileName; //директория файла
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
 }
 
 
@@ -26,52 +26,52 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void compressToZip(){
-    QFileInfo localFile(fileName);
-    QString name = localFile.fileName();
+void compressToZip(){ //Сжималка
+    QFileInfo localFile(fileName); //берем информацию о файле, ниже в переменную запишем её имя, пока нам этого достаточно будет
+    QString name = localFile.fileName();//имя файла, передадим в archive.addLocalFile
 
-    KZip archive(QStringLiteral("test.zip"));
+    KZip archive(QStringLiteral("test.zip")); //Создаем пустой архив
 
-    // Open our archive for writing
-    if (archive.open(QIODevice::WriteOnly)) {
-        // The archive is open, we can now write data
-        archive.addLocalFile(name,name);
-        // Don't forget to close!
-        archive.close();
+    if (archive.open(QIODevice::WriteOnly)) { //Открываем архив
+        archive.addLocalFile(name,name); //Создаем файл внутри архива, метод переносит данные из исходного файла и его имя
+        archive.close(); //Закрываем архив
     }
 }
 
-void uncompressFromZip(){
-    QString file(fileName);
-    KZip archive(file);
+void uncompressFromZip(){ //Разжималка
+    QString localFile(fileName); //берем файл, его название передадим в переменную archive
+    KZip archive(localFile);
 
-    if(!archive.open(QIODevice::ReadOnly)){
+    if(!archive.open(QIODevice::ReadOnly)){ //Открываем архив
         qWarning("Can't open");
         exit(1);
     }
 
-    const KArchiveDirectory *root = archive.directory();
+    const KArchiveDirectory *root = archive.directory(); //Новый файл из архива будет создан в корне приложения
     QString destination = QDir::currentPath();
     bool recursive = true;
     root->copyTo(destination,recursive);
 
-    archive.close();
+    archive.close(); //Не забываем закрыть архив
 }
 
-
-
-
+void MainWindow::on_pushButton_3_clicked()
+{
+    fileName = QFileDialog::getOpenFileName(); //Верхняя кнопка, открывает диалоговое окно для выбора файла
+    ui->label->setText(fileName); //Перенесем в лейбл, чтобы пользователь видел какой файл он открыл
+}
 
 void MainWindow::on_pushButton_clicked()
-{
-    fileName = ui->plainTextEdit->toPlainText();
-    compressToZip();
+{    
+    compressToZip(); //Кнопка сжать
 }
 
 
 void MainWindow::on_pushButton_2_clicked()
-{
-    fileName = ui->plainTextEdit->toPlainText();
-    uncompressFromZip();
+{    
+    uncompressFromZip(); //Кнопка разжать
 }
+
+
+
 
